d517827115e57bfddf26ead50414eced8519b2cc
ivan.sambol <ivan.sambol@d-a-m.cc>
2022-11-15 08:16:33 +0100

    Year to character converter with a reference

---
+++ YearToCharacterConverter/ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Diagnostics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! \n \n");
+
+        Console.WriteLine("Set the reference number:");
+        int refer = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Set the ordinal number of the alphabet that you want to be changed into character:");
+        int res = Convert.ToInt32(Console.ReadLine());
+
+
+
+        while (true)
+        {
+
+            Console.WriteLine(GetCharFromNumber(res, refer));
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Set new number to character:");
+            res = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+
+
+
+    }
+
+    public static string GetCharFromNumber(int number, int referenceNumber)
+    {
+        char[] az = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (Char)i).ToArray();
+        int converter = 0;
+        char resul;
+
+        
+        int rech = referenceNumber % 26;
+
+        converter = Math.Abs(number - rech) % 26;
+        resul = az[converter];
+
+        return resul.ToString();
+    }
+}
